f09092c9e5e986b0b22a9cb197a16c92cfecfa8c
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2016-06-01 23:44:50 +0200

    Code formatting

--- src/razorTools.h
+++ src/razorTools.h
@@ -17,105 +17,110 @@
 
 /*-----------------------------------------------------------------*/
 
-	/* because C doesn't know the bool type,
-	 *an integer based version is created */
-	typedef int bool;
-	#define true 1
-	#define false 0
+/* because C doesn't know the bool type,
+ *an integer based version is created */
+typedef int bool;
+#define true 1
+#define false 0
 
 /*-----------------------------------------------------------------*/
 
-	// streaming mode could be single or continous
-	enum streamingMode {single, continuous};
-	typedef enum streamingMode strMode;
+// streaming mode could be single or continous
+
+enum streamingMode {
+    single, continuous
+};
+typedef enum streamingMode strMode;
 
 /*-----------------------------------------------------------------*/
 
-	// streaming mode could be single or continous
-	enum oFormat {text, binary};
-	typedef enum oFormat outputFormat;
+// streaming mode could be single or continous
+
+enum oFormat {
+    text, binary
+};
+typedef enum oFormat outputFormat;
 
 /*-----------------------------------------------------------------*/
 
-	/* The struggle is, to transform the binary data of the serial stream
-	 * into useful float values. It's just a matter of interpretting ones
-	 * and zeros. So we can use the same memory location and don't care if
-	 * we put four characters inside or one float value, both cases require
-	 * four bytes. We do this using a union structure... */ 
-	union rzrBffr{
-		float f;
-		char ch[4];
-	};
-	typedef union rzrBffr razorBuffer;
+/* The struggle is, to transform the binary data of the serial stream
+ * into useful float values. It's just a matter of interpretting ones
+ * and zeros. So we can use the same memory location and don't care if
+ * we put four characters inside or one float value, both cases require
+ * four bytes. We do this using a union structure... */
+union rzrBffr {
+    float f;
+    char ch[4];
+};
+typedef union rzrBffr razorBuffer;
 
 /*---------------------------------------------------------*/
 
-	struct adjustment{
-		struct termios old_tio;
-		struct termios old_stdio;
-		outputFormat output_Format;
-		strMode streaming_Mode;
-		bool messageOn;
-		bool tio_config_changed;
-		bool stdio_config_changed;
-		char *port;
-    	int tty_fd;
-		int vt_frequency;
-		int waitingTime;
-		bool synchronized;
-		speed_t baudRate;
-	};
+struct adjustment {
+    struct termios old_tio;
+    struct termios old_stdio;
+    outputFormat output_Format;
+    strMode streaming_Mode;
+    bool messageOn;
+    bool tio_config_changed;
+    bool stdio_config_changed;
+    char *port;
+    int tty_fd;
+    int vt_frequency;
+    int waitingTime;
+    bool synchronized;
+    speed_t baudRate;
+};
 
 /*-----------------------------------------------------------------*/
 
-	/* data type to store the yaw, pitch and roll
-	   value of the RazorAHRS tracker */
-	struct razorData{
+/* data type to store the yaw, pitch and roll
+   value of the RazorAHRS tracker */
+struct razorData {
+    /*
+     * Flag that is managed by valueCheck() function
+     * true: the current values don't match the valid range
+     * false: the current values are inside the valid range*/
+    bool data_fail;
 
-		/*
-		 * Flag that is managed by valueCheck() function
-		 * true: the current values don't match the valid range
-		 * false: the current values are inside the valid range*/
-		bool data_fail;
+    /* array that stores the sensor data
+     *
+     * values[0] = Yaw
+     * values[1] = Pitch
+     * values[2] = Roll  */
+    float values[3];
 
-		/* array that stores the sensor data
-		 *
-		 * values[0] = Yaw
-		 * values[1] = Pitch
-	 	 * values[2] = Roll  */
-		float values[3];
-
-		/* union structure to store blocks of 
-		 *  4 Byte received by the tracker (equal 
-		 *  to the size of a single float value.
-		 *  for further information look at 
-		 *  razorTools.h */
-		razorBuffer floatBuffer;
-	};
-
-/*-----------------------------------------------------------------*/
-
-    /* measuring elapsed time:
-	 * During synchronization we need to check the time. Ones to know,
-	 * when we have to send a new request to the tracker/board, second 
-	 * to stop the attempt after a certain amount of time, when we could 
-	 * be shure not having success anymore.   
-	*/
-	long elapsed_ms(struct timeval start, struct timeval end) {
-      return (long) ((end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) / 1000);
-    }
+    /* union structure to store blocks of 
+     *  4 Byte received by the tracker (equal 
+     *  to the size of a single float value.
+     *  for further information look at 
+     *  razorTools.h */
+    razorBuffer floatBuffer;
+};
 
 /*-----------------------------------------------------------------*/
 
-	/* 
-	 * sleep function
-	 */
-	void razorSleep(int milliseconds) {
-		struct timespec waitTime;
-		waitTime.tv_sec = milliseconds / 1000;
-		waitTime.tv_nsec = (milliseconds % 1000) * 1000000;
-		nanosleep(&waitTime, NULL);
-	}
+/* measuring elapsed time:
+ * During synchronization we need to check the time. Ones to know,
+ * when we have to send a new request to the tracker/board, second 
+ * to stop the attempt after a certain amount of time, when we could 
+ * be shure not having success anymore.   
+ */
+long elapsed_ms(struct timeval start, struct timeval end) {
+    return (long) ((end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) / 1000);
+}
+
+/*-----------------------------------------------------------------*/
+
+/* 
+ * sleep function
+ */
+void razorSleep(int milliseconds) {
+    struct timespec waitTime;
+    waitTime.tv_sec = milliseconds / 1000;
+    waitTime.tv_nsec = (milliseconds % 1000) * 1000000;
+    nanosleep(&waitTime, NULL);
+}
 
 /*-----------------------------------------------------------------*/
 
@@ -139,61 +144,43 @@
  * 2. flush type: flushing until no input is readable anymore
  *                --> use flag flushType = 1
  */
-bool razorFlush(struct adjustment* settings, int flush_timeout_ms, int flushType){
-	char singleByte = 'D';
+bool razorFlush(struct adjustment* settings, int flush_timeout_ms, int flushType) {
+    char singleByte = 'D';
 
-	/* variables to store time values
-	 * used to measure how long 
-	 * synchronization takes */
-	struct timeval t0, t1;
-	gettimeofday(&t0, NULL);
+    /* variables to store time values
+     * used to measure how long 
+     * synchronization takes */
+    struct timeval t0, t1;
+    gettimeofday(&t0, NULL);
 
-	//flush to end of line
-	if(flushType == 0){
-		while(singleByte != '\n'){
-	
-			read(settings->tty_fd,&singleByte,1);
+    //flush to end of line
+    if (flushType == 0) {
+        while (singleByte != '\n') {
 
-			// check if time out is reached
-			gettimeofday(&t1, NULL);
-			if (elapsed_ms(t0, t1) > flush_timeout_ms) {
-				printf("INFO: Flushing failed. (time out)\n\r"); // TIME OUT!		
-				return false;
-			}
-		}
-	}
-	//flush full input
-	else if(flushType == 1){
-		while(read(settings->tty_fd,&singleByte,1) > 0){
-			// check if time out is reached
-			gettimeofday(&t1, NULL);
-			if (elapsed_ms(t0, t1) > flush_timeout_ms) {
-				printf("INFO: Flushing failed. (time out)\n\r"); // TIME OUT!		
-				return false;
-			}
-		}
-	}
-	else{
-		printf("INFO: Flushing failed. (invalid flushing type flag)\n\r");
-		return false;		
-	}
-	return true;
+            read(settings->tty_fd, &singleByte, 1);
+
+            // check if time out is reached
+            gettimeofday(&t1, NULL);
+            if (elapsed_ms(t0, t1) > flush_timeout_ms) {
+                printf("INFO: Flushing failed. (time out)\n\r"); // TIME OUT!		
+                return false;
+            }
+        }
+    }        //flush full input
+    else if (flushType == 1) {
+        while (read(settings->tty_fd, &singleByte, 1) > 0) {
+            // check if time out is reached
+            gettimeofday(&t1, NULL);
+            if (elapsed_ms(t0, t1) > flush_timeout_ms) {
+                printf("INFO: Flushing failed. (time out)\n\r"); // TIME OUT!		
+                return false;
+            }
+        }
+    } else {
+        printf("INFO: Flushing failed. (invalid flushing type flag)\n\r");
+        return false;
+    }
+    return true;
 }
-
-/*-----------------------------------------------------------------*/
-
-	/* Compares two strings. If the strings are literally are equal,
-	 * it returns true (1), otherwise false (0)
-	 * !!! out dated - not used anymore !!!
-	*/
-	bool stringCompare(char* s1, char* s2, int length){
-		for(int i = 0; i < length; i++){
-			if(s1[i] != s2[i]){
-				return false;
-			}
-		}
-		return true;	
-	}
-
 
 #endif // RAZORTOOLS_H
--- src/virtualTracker.c
+++ src/virtualTracker.c
@@ -8,221 +8,185 @@
 #include <fcntl.h>
 #include <termios.h>
 
-#include <string.h> // needed for memset
+#include <string.h>
 
 #include <errno.h>
 
 #include "config.h"
 #include "razorTools.h"
 
-
-/*---------------------------------------------------------*/
-
-struct vt_adjustment{
-	struct termios old_tio;
-	struct termios old_stdio;
-	outputFormat output_Format;
-	char *vt_port;
+struct vt_adjustment {
+    struct termios old_tio;
+    struct termios old_stdio;
+    outputFormat output_Format;
+    char *vt_port;
     int tty_fd;
-	int vt_frequency;
-	int waitingTime;
-	speed_t vt_baudRate;
+    int vt_frequency;
+    int waitingTime;
+    speed_t vt_baudRate;
 };
 
-/*---------------------------------------------------------*/
-
-int hertzToMilliseconds(int frequency){
-	return (1000 / frequency);
+int hertzToMilliseconds(int frequency) {
+    return (1000 / frequency);
 }
 
-/*---------------------------------------------------------*/
-
-void resetIOConfiguration(struct vt_adjustment *setting){
-	// reactivating previous configurations of tty_fd and the STDOUT_FIELNO
-	tcsetattr(setting->tty_fd, TCSANOW, &setting->old_tio);
-	tcsetattr(STDOUT_FILENO, TCSANOW, &setting->old_stdio);
+void resetIOConfiguration(struct vt_adjustment *setting) {
+    // reactivating previous configurations of tty_fd and the STDOUT_FIELNO
+    tcsetattr(setting->tty_fd, TCSANOW, &setting->old_tio);
+    tcsetattr(STDOUT_FILENO, TCSANOW, &setting->old_stdio);
     close(setting->tty_fd);
 }
 
-/*---------------------------------------------------------*/
+bool prepareIOConfiguration(struct vt_adjustment *setting) {
+    //saving current termios configurations of STDOUT_FILENO
+    if (tcgetattr(STDOUT_FILENO, &setting->old_stdio) != 0) {
+        printf("tcgetattr(fd, &old_stdtio) failed\n");
+        return false;
+    }
 
-bool prepareIOConfiguration(struct vt_adjustment *setting){
+    stdio_Config();
 
-	//saving current termios configurations of STDOUT_FILENO
-	if (tcgetattr(STDOUT_FILENO, &setting->old_stdio) != 0) {
-       	printf("tcgetattr(fd, &old_stdtio) failed\n");
-       	return false;
-   	}
+    setting->tty_fd = open(setting->vt_port, O_RDWR | O_NONBLOCK);
 
-	stdio_Config();
+    printf("Connecting to: %s (%d) \r\n", setting->vt_port, setting->tty_fd);
 
-    setting->tty_fd = open(setting->vt_port, O_RDWR | O_NONBLOCK);      
+    //saving current termios configurations of tty_fd
+    if (tcgetattr(setting->tty_fd, &setting->old_tio) != 0) {
+        printf("tcgetattr(fd, &old_tio) failed\n");
+        resetIOConfiguration(setting);
+        return false;
+    }
 
-	printf("Connecting to: %s (%d) \r\n", setting->vt_port, setting->tty_fd );		
+    tio_Config(setting->tty_fd, setting->vt_baudRate);
 
-	//saving current termios configurations of tty_fd
-	if (tcgetattr(setting->tty_fd, &setting->old_tio) != 0) {
-		printf("tcgetattr(fd, &old_tio) failed\n");
-		resetIOConfiguration(setting);
-       	return false;
-    }		
-
-	tio_Config(setting->tty_fd, setting->vt_baudRate);
-
-	return true;
+    return true;
 
 }
 
-/*---------------------------------------------------------*/
+void sendToken(struct vt_adjustment *setting, unsigned char *id) {
+    unsigned char *synch;
+    synch = calloc(10, sizeof (unsigned char));
 
-void sendToken(struct vt_adjustment *setting, unsigned char *id){
-	
-	unsigned char *synch;
-	synch = calloc(10, sizeof(unsigned char) );
+    synch[0] = '#';
+    synch[1] = 'S';
+    synch[2] = 'Y';
+    synch[3] = 'N';
+    synch[4] = 'C';
+    synch[5] = 'H';
+    synch[6] = id[0];
+    synch[7] = id[1];
+    synch[8] = '\r';
+    synch[9] = '\n';
 
-	synch[0] = '#';
-	synch[1] = 'S';
-	synch[2] = 'Y';
-	synch[3] = 'N';
-	synch[4] = 'C';
-	synch[5] = 'H';
-	synch[6] = id[0];
-	synch[7] = id[1];
-	synch[8] = '\r';
-	synch[9] = '\n';
+    printf("sending: %s", synch);
 
-	printf("sending: %s", synch);
+    write(setting->tty_fd, synch, 10);
 
-	write(setting->tty_fd, synch, 10);
-
-	free(synch);
-
+    free(synch);
 }
 
-/*---------------------------------------------------------*/
+bool loopOrBreak(char stopCharacter) {
+    unsigned char console = 'D';
 
-bool loopOrBreak(char stopCharacter){
-	unsigned char console = 'D';
+    if (read(STDIN_FILENO, &console, 1) > 0 && console == stopCharacter) {
+        return false;
+    }
 
-	if (read(STDIN_FILENO,&console,1)>0){  
-		if (console == stopCharacter) return false;
-	}
-	
-	return true;
+    return true;
 }
 
-/*---------------------------------------------------------*/
+int virtualTracker(int frequency, speed_t baudRate, char* port) {
+    struct vt_adjustment *setting;
+    setting = (struct vt_adjustment*) calloc(1, sizeof (struct vt_adjustment));
+    setting->vt_port = port;
+    setting->vt_baudRate = baudRate;
+    setting->vt_frequency = frequency;
+    setting->output_Format = text;
+    setting->waitingTime = hertzToMilliseconds(frequency);
+    if (prepareIOConfiguration(setting) == false) return -1;
 
-int virtualTracker(int frequency, speed_t baudRate, char* port){
+    razorBuffer *floatBuffer;
+    floatBuffer = (razorBuffer*) calloc(1, sizeof (razorBuffer));
 
-	struct vt_adjustment *setting;
-	setting = (struct vt_adjustment*) calloc(1, sizeof(struct vt_adjustment) );
-	setting->vt_port = port;
-	setting->vt_baudRate = baudRate;
-	setting->vt_frequency = frequency;
-	setting->output_Format = text;
-	setting->waitingTime = hertzToMilliseconds(frequency);
-	if(prepareIOConfiguration(setting) == false) return -1;
+    float arr[3] = {0, 0, 0};
 
-	razorBuffer *floatBuffer;
-	floatBuffer = (razorBuffer*) calloc(1, sizeof(razorBuffer) );
+    unsigned char singleByte = 'D';
+    unsigned char *id;
+    id = calloc(2, sizeof (int));
 
-	float arr[3] = {0,0,0};
+    int readBytes = 0;
+    printf("\nPress spacebar to stop virtual Razor.\n\n");
+    while (loopOrBreak(' ')) {
+        readBytes = read(setting->tty_fd, &singleByte, 1);
 
-	unsigned char singleByte = 'D';
-	unsigned char *id;
-	id = calloc(2, sizeof(int) );
+        // INPUT CHECK
+        if ((readBytes == 1) && (singleByte = '#')) {
+            printf("\r\nreading: %c", singleByte);
+            readBytes = read(setting->tty_fd, &singleByte, 1);
 
+            if ((readBytes == 1) && (singleByte == 's')) {
+                printf("%c\n\r", singleByte);
 
-	int readBytes = 0;
+                // Synch Request
+                read(setting->tty_fd, &singleByte, 1);
+                id[0] = singleByte;
+                read(setting->tty_fd, &singleByte, 1);
+                id[1] = singleByte;
 
-	printf("\nPress spacebar to stop virtual Razor.\n\n");
-	while ( loopOrBreak(' ') ) {
+                sendToken(setting, id);
 
-		readBytes = read(setting->tty_fd,&singleByte,1);
+            } else if ((readBytes == 1) && (singleByte == 'o')) {
+                printf("%c", singleByte);
+                readBytes = read(setting->tty_fd, &singleByte, 1);
 
+                printf("%c\r\n", singleByte);
 
-		// INPUT CHECK
-       	if( (readBytes == 1) && (singleByte = '#') ){
-	
-			printf("\r\nreading: %c", singleByte);			
+                // set output mode to binary (3 * 4 bytes = three floating point values)
+                if ((readBytes == 1) && (singleByte == 'b')) setting->output_Format = binary;
+                    // set output mode to string (text)
+                else if ((readBytes == 1) && (singleByte == 't')) setting->output_Format = text;
+            }
+        }
 
-			readBytes = read(setting->tty_fd,&singleByte,1);
+        // incrementing output angles
+        if (arr[0] < 180) arr[0] += 10;
+        else if (arr[1] < 180) arr[1] += 10;
+        else if (arr[2] < 180) arr[2] += 10;
+        else {
+            arr[0] = 0;
+            arr[1] = 0;
+            arr[2] = 0;
+        }
 
-	       	if( (readBytes == 1) && (singleByte == 's') ){
-			
-				printf("%c\n\r", singleByte);			
-	
-				// Synch Request
-				read(setting->tty_fd,&singleByte,1);
-				id[0] = singleByte;
-				read(setting->tty_fd,&singleByte,1);
- 				id[1] = singleByte;
+        // BINARY OUTPUT MODE
+        if (setting->output_Format == binary) {
+            //printf("\rYAW: %.1f\t PITCH: %.1f\t ROLL: %.1f\n", arr[0], arr[1], arr[2]);				
+            write(setting->tty_fd, arr, 12);
+        }// TEXT OUTPUT MODE
+        else {
+            // snprintf() returns the number of characters it would have written
+            ssize_t len = snprintf(NULL, 0, "#YPR=%.2f,%.2f,%.2f\r\n", arr[0], arr[1], arr[2]);
 
- 				sendToken(setting, id);
-				
-			}
-			else if( (readBytes == 1) && (singleByte == 'o') ) {
+            // allocate storage for a string
+            char *output = malloc(len + 1);
 
-				printf("%c", singleByte);			
+            if (output != NULL) {
+                // generate String
+                sprintf(output, "#YPR=%.2f,%.2f,%.2f\r\n", arr[0], arr[1], arr[2]);
 
-				readBytes = read(setting->tty_fd,&singleByte,1);
+                //printf("%s\n", output);
 
-				printf("%c\r\n", singleByte);			
+                write(setting->tty_fd, output, len);
+                free(output);
+            }
+        }
+        razorSleep(setting->waitingTime);
+    }
 
-				// set output mode to binary (3 * 4 bytes = three floating point values)
-				if( (readBytes == 1) && (singleByte == 'b') ) setting->output_Format = binary;
-				// set output mode to string (text)
-				else if( (readBytes == 1) && (singleByte == 't') ) setting->output_Format = text;			
+    resetIOConfiguration(setting);
+    free(floatBuffer);
+    free(setting);
+    free(id);
 
-			}			
-
-		}
-
-		// incrementing output angles
-		if(arr[0] < 180) arr[0] += 10;
-		else if(arr[1] < 180) arr[1] += 10;
-		else if(arr[2] < 180) arr[2] += 10;
-		else {arr[0] = 0; arr[1] = 0; arr[2] = 0;}
-
-		// BINARY OUTPUT MODE
-		if(setting->output_Format == binary){
-			//printf("\rYAW: %.1f\t PITCH: %.1f\t ROLL: %.1f\n", arr[0], arr[1], arr[2]);				
-			write(setting->tty_fd, arr, 12);
-		}
-		// TEXT OUTPUT MODE
-		else{
-			// snprintf() returns the number of characters it would have written
-			ssize_t len = snprintf(NULL, 0, "#YPR=%.2f,%.2f,%.2f\r\n", arr[0], arr[1], arr[2]);
-
-			// allocate storage for a string
-			char *output = malloc( len+1 );
-
-			if( output != NULL ) {
-				// generate String
-				sprintf(output, "#YPR=%.2f,%.2f,%.2f\r\n", arr[0], arr[1], arr[2]);
-
-				//printf("%s\n", output);
-
-				write(setting->tty_fd, output, len);
-				free(output);
-			}
-		}
-	
-
-		razorSleep(setting->waitingTime);
-
-	}
-
-	resetIOConfiguration(setting);
-	free(floatBuffer);
-	free(setting);
-	free(id);
-
-	return 0;
-
+    return 0;
 }
-
-/*---------------------------------------------------------*/
-
-
