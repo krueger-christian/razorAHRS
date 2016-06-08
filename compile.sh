94d93384e7580faa197c06cf07ba1b549158d6b6
krueger.christian <christian.r.krueger@campus.tu-berlin.de>
2016-06-08 17:53:57 +0200

    update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,6 @@
 gcc -Wall src/example_razorAHRS.c -o bin/example_razorAHRS
 
 gcc -Wall src/example_virtualTracker.c -o bin/example_virtualTracker
+
+gcc -Wall src/example_razorAHRS_calibration.c -o bin/example_razorAHRS_calibration
+
