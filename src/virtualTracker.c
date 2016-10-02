ea2ccdee567f48b3c27568c594a62cc7a28040d8
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2016-10-02 11:44:29 -0700

    Code cleanup

--- src/virtualTracker.c
+++ src/virtualTracker.c
@@ -31,26 +31,32 @@ struct vt_adjustment {
   speed_t vt_baudRate;
 };
 
-    /* measuring elapsed time:
-     * During synchronization we need to check the time. Ones to know,
-     * when we have to send a new request to the tracker/board, second 
-     * to stop the attempt after a certain amount of time, when we could 
-     * be shure not having success anymore.   
-     */
-long elapsed_ms (struct timeval start, struct timeval end) {
+/** 
+Computes the elapsed time in ms.
+*/
+unsigned long elapsed_ms (struct timeval start, struct timeval end) {
   return (long) ((end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) / 1000);
 }
 
-void razorSleep (int milliseconds) {
-  struct timespec waitTime;
-  waitTime.tv_sec = milliseconds / 1000;
-  waitTime.tv_nsec = (milliseconds % 1000) * 1000000;
-  nanosleep (&waitTime, NULL);
+void send_token_sync (struct vt_adjustment setting, unsigned char sync_id1, unsigned char sync_id2) {
+  unsigned char sync_message[] = {'#', 'S', 'Y', 'N', 'C', 'H', sync_id1, sync_id2, '\r', '\n'};
+  printf ("sending: %s", sync_message);
+
+  write (setting.tty_fd, sync_message, 10); //TODO: Replace 10
 }
 
-void stdio_config () {
-  struct termios stdio;
+bool virtualTracker (unsigned int frequency, speed_t baudRate, char *port) {
+  //Setup socket
+  struct vt_adjustment setting;
+  memset (&setting, 0, sizeof (setting));
+  setting.vt_port = port;
+  setting.vt_baudRate = baudRate;
+  setting.vt_frequency = frequency;
+  setting.output_Format = STREAMINGFORMAT_ASCII;
+  setting.output_Mode = STREAMINGMODE_CONTINOUS;
+  setting.waitingTime = 1000 / frequency;
 
+  struct termios stdio;
   memset (&stdio, 0, sizeof (stdio));
   stdio.c_iflag = 0;
   stdio.c_oflag = 0;
@@ -61,12 +67,10 @@ void stdio_config () {
   tcsetattr (STDOUT_FILENO, TCSANOW, &stdio);
   tcsetattr (STDOUT_FILENO, TCSAFLUSH, &stdio);
   fcntl (STDIN_FILENO, F_SETFL, O_NONBLOCK);    // make the reads non-blocking
-}
 
-void tio_config (int tty_fd, speed_t baudRate) {
+  setting.tty_fd = open (setting.vt_port, O_RDWR | O_NONBLOCK);
 
   struct termios tio;
-
   memset (&tio, 0, sizeof (tio));
   tio.c_iflag = 0;
   tio.c_oflag = 0;
@@ -74,197 +78,79 @@ void tio_config (int tty_fd, speed_t baudRate) {
   tio.c_lflag = 0;
   tio.c_cc[VMIN] = 1;
   tio.c_cc[VTIME] = 10;
-  cfsetospeed (&tio, baudRate); // 57600 baud
-  cfsetispeed (&tio, baudRate); // 57600 baud
-  tcsetattr (tty_fd, TCSANOW, &tio);
-}
+  cfsetospeed (&tio, setting.vt_baudRate);
+  cfsetispeed (&tio, setting.vt_baudRate);
+  tcsetattr (setting.tty_fd, TCSANOW, &tio);
 
-/*----------------------------------------------------------------------------------------------------*/
+  printf ("Connecting to: %s (%d) \r\n", setting.vt_port, setting.tty_fd);
 
-int hertzToMilliseconds (int frequency) {
-  return (1000 / frequency);
-}
+  //Prepare for writing
 
-/*----------------------------------------------------------------------------------------------------*/
+  unsigned char read_byte;
+  unsigned int read_byte_length = 0;
 
-void resetIOConfiguration (struct vt_adjustment *setting) {
-  // reactivating previous configurations of tty_fd and the STDOUT_FIELNO
-  tcsetattr (setting->tty_fd, TCSANOW, &setting->old_tio);
-  tcsetattr (STDOUT_FILENO, TCSANOW, &setting->old_stdio);
-  close (setting->tty_fd);
-}
-
-/*----------------------------------------------------------------------------------------------------*/
-
-bool prepareIOConfiguration (struct vt_adjustment *setting) {
-  //saving current termios configurations of STDOUT_FILENO
-  if (tcgetattr (STDOUT_FILENO, &setting->old_stdio) != 0) {
-    printf ("tcgetattr(fd, &old_stdtio) failed\n");
-    return false;
-  }
-
-  stdio_config ();
-
-  setting->tty_fd = open (setting->vt_port, O_RDWR | O_NONBLOCK);
-
-  printf ("Connecting to: %s (%d) \r\n", setting->vt_port, setting->tty_fd);
-
-  //saving current termios configurations of tty_fd
-  if (tcgetattr (setting->tty_fd, &setting->old_tio) != 0) {
-    printf ("tcgetattr(fd, &old_tio) failed\n");
-    resetIOConfiguration (setting);
-    return false;
-  }
-
-  tio_config (setting->tty_fd, setting->vt_baudRate);
-
-  return true;
-
-}
-
-/*----------------------------------------------------------------------------------------------------*/
-
-void send_token_sync (struct vt_adjustment *setting, unsigned char *id) {
-  unsigned char *synch;
-  synch = calloc (10, sizeof (unsigned char));
-
-  synch[0] = '#';
-  synch[1] = 'S';
-  synch[2] = 'Y';
-  synch[3] = 'N';
-  synch[4] = 'C';
-  synch[5] = 'H';
-  synch[6] = id[0];
-  synch[7] = id[1];
-  synch[8] = '\r';
-  synch[9] = '\n';
-  printf ("sending: %s", synch);
-
-  write (setting->tty_fd, synch, 10);
-
-  free (synch);
-}
-
-/*----------------------------------------------------------------------------------------------------*/
-
-bool loopOrBreak (char stopCharacter) {
-  unsigned char console = 'D';
-
-  if (read (STDIN_FILENO, &console, 1) > 0 && console == stopCharacter) {
-    return false;
-  }
-
-  return true;
-}
-
-/*----------------------------------------------------------------------------------------------------*/
-
-int virtualTracker (int frequency, speed_t baudRate, char *port) {
-  struct vt_adjustment *setting;
-  setting = (struct vt_adjustment *) calloc (1, sizeof (struct vt_adjustment));
-  setting->vt_port = port;
-  setting->vt_baudRate = baudRate;
-  setting->vt_frequency = frequency;
-  setting->output_Format = STREAMINGFORMAT_ASCII;
-  setting->output_Mode = STREAMINGMODE_CONTINOUS;
-  setting->waitingTime = hertzToMilliseconds (frequency);
-  if (prepareIOConfiguration (setting) == false)
-    return -1;
-
-  float arr[3] = { 0, 0, 0 };
-
-  unsigned char STREAMINGMODE_SINGLEByte = 'D';
-  unsigned char *id;
-  id = calloc (2, sizeof (int));
-
-  int readBytes = 0;
   bool request = false;
 
-  printf ("\nPress spacebar to stop virtual Razor.\n\n");
-  while (loopOrBreak (' ')) {
-    readBytes = read (setting->tty_fd, &STREAMINGMODE_SINGLEByte, 1);
+  while (true) {
+    read_byte_length = read (setting.tty_fd, &read_byte, 1);
 
     // INPUT CHECK
-    if ((readBytes == 1) && (STREAMINGMODE_SINGLEByte = '#')) {
-      printf ("\r\nreading: %c", STREAMINGMODE_SINGLEByte);
-      readBytes = read (setting->tty_fd, &STREAMINGMODE_SINGLEByte, 1);
+    if ((read_byte_length == 1) && (read_byte = '#')) {
+      printf ("\r\nreading: %c", read_byte);
+      read_byte_length = read (setting.tty_fd, &read_byte, 1);
 
-      if ((readBytes == 1) && (STREAMINGMODE_SINGLEByte == 's')) {
-        printf ("%c\n\r", STREAMINGMODE_SINGLEByte);
+      if ((read_byte_length == 1) && (read_byte == 's')) {
+        printf ("%c\n\r", read_byte);
 
-        // Synch Request
-        read (setting->tty_fd, &STREAMINGMODE_SINGLEByte, 1);
-        id[0] = STREAMINGMODE_SINGLEByte;
-        read (setting->tty_fd, &STREAMINGMODE_SINGLEByte, 1);
-        id[1] = STREAMINGMODE_SINGLEByte;
+        // Sync Request
+        unsigned char sync_id[] = {' ', ' '};
+        read (setting.tty_fd, &sync_id, 2);
 
-        send_token_sync (setting, id);
+        send_token_sync (setting, sync_id[0], sync_id[1]);
 
-      } else if ((readBytes == 1) && (STREAMINGMODE_SINGLEByte == 'o')) {
-        printf ("%c", STREAMINGMODE_SINGLEByte);
-        readBytes = read (setting->tty_fd, &STREAMINGMODE_SINGLEByte, 1);
+      } else if ((read_byte_length == 1) && (read_byte == 'o')) {
+        printf ("%c", read_byte);
+        read_byte_length = read (setting.tty_fd, &read_byte, 1);
 
-        printf ("%c\r\n", STREAMINGMODE_SINGLEByte);
+        printf ("%c\r\n", read_byte);
 
         // set output mode to STREAMINGFORMAT_BINARY (3 * 4 bytes = three floating point values)
-        if ((readBytes == 1) && (STREAMINGMODE_SINGLEByte == 'b'))
-          setting->output_Format = STREAMINGFORMAT_BINARY;
+        if ((read_byte_length == 1) && (read_byte == 'b'))
+          setting.output_Format = STREAMINGFORMAT_BINARY;
         // set output mode to string (STREAMINGFORMAT_ASCII)
-        else if ((readBytes == 1) && (STREAMINGMODE_SINGLEByte == 't'))
-          setting->output_Format = STREAMINGFORMAT_ASCII;
-        else if ((readBytes == 1) && (STREAMINGMODE_SINGLEByte == '1'))
-          setting->output_Mode = STREAMINGMODE_CONTINOUS;
-        else if ((readBytes == 1) && (STREAMINGMODE_SINGLEByte == '0'))
-          setting->output_Mode = STREAMINGMODE_SINGLE;
-      } else if ((readBytes == 1) && (STREAMINGMODE_SINGLEByte == 'f')) {
-        printf ("%c", STREAMINGMODE_SINGLEByte);
+        else if ((read_byte_length == 1) && (read_byte == 't'))
+          setting.output_Format = STREAMINGFORMAT_ASCII;
+        else if ((read_byte_length == 1) && (read_byte == '1'))
+          setting.output_Mode = STREAMINGMODE_CONTINOUS;
+        else if ((read_byte_length == 1) && (read_byte == '0'))
+          setting.output_Mode = STREAMINGMODE_SINGLE;
+      } else if ((read_byte_length == 1) && (read_byte == 'f')) {
+        printf ("%c", read_byte);
         request = true;
       }
     }
-    // incrementing output angles
-    if (arr[0] < 180)
-      arr[0] += 10;
-    else if (arr[1] < 180)
-      arr[1] += 10;
-    else if (arr[2] < 180)
-      arr[2] += 10;
-    else {
-      arr[0] = 0;
-      arr[1] = 0;
-      arr[2] = 0;
-    }
 
-    if ((setting->output_Mode == STREAMINGMODE_CONTINOUS) || (request == true)) {
-      // STREAMINGFORMAT_BINARY OUTPUT MODE
-      if (setting->output_Format == STREAMINGFORMAT_BINARY) {
-        //printf("\rYAW: %.1f\t PITCH: %.1f\t ROLL: %.1f\n",\arr[0], arr[1], arr[2]);
-        write (setting->tty_fd, arr, 12);
-      }                         // STREAMINGFORMAT_ASCII OUTPUT MODE
-      else {
-        // snprintf() returns the number of characters it would have written
-        ssize_t len = snprintf (NULL, 0, "#YPR=%.2f,%.2f,%.2f\r\n", arr[0], arr[1], arr[2]);
+    if (setting.output_Mode == STREAMINGMODE_CONTINOUS || request == true) {
+      float data[3];
 
-        // allocate storage for a string
-        char *output = malloc (len + 1);
+      ssize_t len = snprintf (NULL, 0, "#YPR=%3.2f,%3.2f,%3.2f\r\n", data[0], data[1], data[2]);
+      char output[len];
+      sprintf (output, "#YPR=%3.2f,%3.2f,%3.2f\r\n", data[0], data[1], data[2]);
 
-        if (output != NULL) {
-          // generate String
-          sprintf (output, "#YPR=%.2f,%.2f,%.2f\r\n", arr[0], arr[1], arr[2]);
-
-          //printf("%s\n", output);
-
-          write (setting->tty_fd, output, len);
-          free (output);
-        }
+      switch (setting.output_Format) {
+        case STREAMINGFORMAT_BINARY:
+          write (setting.tty_fd, data, 12); //12? It should be shorter, or?
+          break;
+        case STREAMINGFORMAT_ASCII:
+          write (setting.tty_fd, output, len);
+        break;
       }
       request = false;
     }
-    razorSleep (setting->waitingTime);
+    usleep (setting.waitingTime * 1000);
   }
 
-  resetIOConfiguration (setting);
-  free (setting);
-  free (id);
+  close (setting.tty_fd);
 
   return true;
 }
