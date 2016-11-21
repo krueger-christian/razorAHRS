62dd35743463e7f9eba2e916573ef627113801a0
krueger-christian <christian.r.krueger@campus.tu-berlin.de>
2016-11-21 14:22:43 +0100

    Add files via upload

---
+++ Razor_AHRS_v1-5/Output.ino
@@ -0,0 +1,271 @@
+/* This file is part of the Razor AHRS Firmware */
+
+// adding a number to the 32bit output data format
+long addingBits(int number, long  bitarray, int startbit){
+  if(number > 360) return bitarray;
+
+  long tmp = (long) number;
+  tmp <<= startbit;
+
+  bitarray |= tmp;
+
+  return bitarray;
+} 
+
+long wrappingValues(int value_1, int value_2, int value_3){
+
+  int checksum = 0;
+  if(value_1 & 1) checksum++;
+  if(value_2 & 1) checksum++;
+  if(value_3 & 1) checksum++;
+
+  long bitarray = 0;
+  bitarray = addingBits(value_1 + 180, bitarray, 22);
+  bitarray = addingBits(value_2 + 180, bitarray, 12);
+  bitarray = addingBits(value_3 + 180, bitarray,  2);
+  bitarray = addingBits(checksum, bitarray, 0);
+
+  return bitarray;
+}
+
+
+// Output angles: yaw, pitch, roll
+void output_angles()
+{
+  float final_yaw = TO_DEG(yaw) - yaw_up;
+  float final_pitch = TO_DEG(pitch) - pitch_up;
+  float final_roll = TO_DEG(roll) - roll_up;
+  
+  if (output_format == OUTPUT__FORMAT_BINARY)
+  {
+    float ypr[3];  
+    ypr[0] = final_yaw; // TO_DEG(yaw);
+    ypr[1] = final_pitch; // TO_DEG(pitch);
+    ypr[2] = final_roll; // TO_DEG(roll);
+    Serial.write((byte*) ypr, 12);  // No new-line
+  }
+  if (output_format == OUTPUT__FORMAT_FOURBYTE)
+  {
+    buff.l = wrappingValues( (int) final_yaw, (int) final_pitch, (int) final_roll);
+    Serial.write(buff.b, 4);  // No new-lines
+  }
+  else if (output_format == OUTPUT__FORMAT_TEXT)
+  {
+      Serial.print("#YPR=");
+      Serial.print(final_yaw); Serial.print(","); // yaw, around z-axis
+      Serial.print(final_pitch); Serial.print(","); // pitch, around y-axis
+      Serial.print(final_roll); Serial.println(); // roll, around x-axis
+  }
+}
+
+void output_calibration(int calibration_sensor)
+{
+  if(output_format == OUTPUT__FORMAT_BINARY){
+
+    char mode[2];
+    if (calibration_sensor == 0)  // Accelerometer
+    {
+      if(calibration_step == 0) // finding x-maximum
+      {
+        if (accel[0] > accel_max[0]) accel_max[0] = accel[0];
+        //buff.f = accel_max[0];
+        //buff.f = 15.5;
+        Serial.write((byte*) buff.b, 4);
+      }
+      else if(calibration_step == 1) // finding x-minimum
+      {
+        if (accel[0] < accel_min[0]) accel_min[0] = accel[0];
+        //buff.f = accel_min[0];
+        buff.f = -18.3;
+        Serial.write((byte*) buff.b, 4);
+      }
+      else if(calibration_step == 2) // finding y-maximum
+      {
+        if (accel[1] > accel_max[1]) accel_max[1] = accel[1];
+        buff.f = accel_max[1];
+        Serial.write((byte*) buff.b, 4);
+      }
+      else if(calibration_step == 3) // finding y-minimum
+      {
+        if (accel[1] < accel_min[1]) accel_min[1] = accel[1];
+        buff.f = accel_min[1]; 
+        Serial.write((byte*) buff.b, 4);
+      }
+      if(calibration_step == 4) // finding z-maximum
+      {
+        if (accel[2] > accel_max[2]) accel_max[2] = accel[2];
+        buff.f = accel_max[2];
+        Serial.write((byte*) buff.b, 4);
+      }
+      else if(calibration_step == 5) // finding z-minimum
+      {
+        if (accel[2] < accel_min[2]) accel_min[2] = accel[2];
+        buff.f = accel_min[2];
+        Serial.write((byte*) buff.b, 4);
+      }
+    }
+    else if (calibration_sensor == 1)  // Magnetometer
+    {
+      if(calibration_step == 0) // finding x-maximum
+      {
+        if (magnetom[0] > magnetom_max[0]) magnetom_max[0] = magnetom[0];
+        buff.f = magnetom_max[0];
+        Serial.write((byte*) buff.b, 4);
+      }
+      else if(calibration_step == 1) // finding x-minimum
+      {
+        if (magnetom[0] < magnetom_min[0]) magnetom_min[0] = magnetom[0];
+        buff.f = magnetom_min[0];
+        Serial.write((byte*) buff.b, 4);
+      }
+      else if(calibration_step == 2) // finding y-maximum
+      {
+        if (magnetom[1] > magnetom_max[1]) magnetom_max[1] = magnetom[1];
+        buff.f = magnetom_max[1];
+        Serial.write((byte*) buff.b, 4);
+      }
+      else if(calibration_step == 3) // finding y-minimum
+      {
+        if (magnetom[1] < magnetom_min[1]) magnetom_min[1] = magnetom[1];
+        buff.f = magnetom_min[1];
+        Serial.write((byte*) buff.b, 4);
+      }
+      else if(calibration_step == 4) // finding z-maximum
+      {
+        if (magnetom[2] > magnetom_max[2]) magnetom_max[2] = magnetom[2];
+        buff.f = magnetom_max[2];
+        Serial.write((byte*) buff.b, 4);
+      }
+      else if(calibration_step == 5) // finding z-minimum
+      {
+        if (magnetom[2] < magnetom_min[2]) magnetom_min[2] = magnetom[2];
+        buff.f = magnetom_min[2];
+        Serial.write((byte*) buff.b, 4);
+      }
+    }
+    else if (calibration_sensor == 2)  // Gyroscope
+    {
+      // Average gyro values
+      for (int i = 0; i < 3; i++)
+        gyro_average[i] += gyro[i];
+      gyro_num_samples++;
+
+      float gyr_val[3];
+      gyr_val[0] = (gyro_average[0] / (float) gyro_num_samples);
+      gyr_val[1] = (gyro_average[1] / (float) gyro_num_samples);
+      gyr_val[2] = (gyro_average[2] / (float) gyro_num_samples);
+      
+      Serial.write((byte*) gyr_val, 12);
+    }
+  }
+  else if(output_format == OUTPUT__FORMAT_TEXT)
+  {
+    if (calibration_sensor == 0)  // Accelerometer
+    {
+      // Output MIN/MAX values
+      Serial.print("accel x,y,z (min/max) = ");
+      for (int i = 0; i < 3; i++) {
+        if (accel[i] < accel_min[i]) accel_min[i] = accel[i];
+        if (accel[i] > accel_max[i]) accel_max[i] = accel[i];
+        Serial.print(accel_min[i]);
+        Serial.print("/");
+        Serial.print(accel_max[i]);
+        if (i < 2) Serial.print("  ");
+        else Serial.println();
+      }
+    }
+    else if (calibration_sensor == 1)  // Magnetometer
+    {
+      // Output MIN/MAX values
+      Serial.print("magn x,y,z (min/max) = ");
+      for (int i = 0; i < 3; i++) {
+        if (magnetom[i] < magnetom_min[i]) magnetom_min[i] = magnetom[i];
+        if (magnetom[i] > magnetom_max[i]) magnetom_max[i] = magnetom[i];
+        Serial.print(magnetom_min[i]);
+        Serial.print("/");
+        Serial.print(magnetom_max[i]);
+        if (i < 2) Serial.print("  ");
+        else Serial.println();
+      }
+    }
+    else if (calibration_sensor == 2)  // Gyroscope
+    {
+      // Average gyro values
+      for (int i = 0; i < 3; i++)
+        gyro_average[i] += gyro[i];
+      gyro_num_samples++;
+        
+      // Output current and averaged gyroscope values
+      Serial.print("gyro x,y,z (current/average) = ");
+      for (int i = 0; i < 3; i++) {
+        Serial.print(gyro[i]);
+        Serial.print("/");
+        Serial.print(gyro_average[i] / (float) gyro_num_samples);
+        if (i < 2) Serial.print("  ");
+        else Serial.println();
+      }
+    }
+  }
+}
+
+void output_sensors_text(char raw_or_calibrated)
+{
+  Serial.print("#A-"); Serial.print(raw_or_calibrated); Serial.print('=');
+  Serial.print(accel[0]); Serial.print(",");
+  Serial.print(accel[1]); Serial.print(",");
+  Serial.print(accel[2]); Serial.println();
+
+  Serial.print("#M-"); Serial.print(raw_or_calibrated); Serial.print('=');
+  Serial.print(magnetom[0]); Serial.print(",");
+  Serial.print(magnetom[1]); Serial.print(",");
+  Serial.print(magnetom[2]); Serial.println();
+
+  Serial.print("#G-"); Serial.print(raw_or_calibrated); Serial.print('=');
+  Serial.print(gyro[0]); Serial.print(",");
+  Serial.print(gyro[1]); Serial.print(",");
+  Serial.print(gyro[2]); Serial.println();
+}
+
+void output_sensors_binary()
+{
+  Serial.write((byte*) accel, 12);
+  Serial.write((byte*) magnetom, 12);
+  Serial.write((byte*) gyro, 12);
+}
+
+void output_sensors()
+{
+  if (output_mode == OUTPUT__MODE_SENSORS_RAW)
+  {
+    if (output_format == OUTPUT__FORMAT_BINARY)
+      output_sensors_binary();
+    else if (output_format == OUTPUT__FORMAT_TEXT)
+      output_sensors_text('R');
+  }
+  else if (output_mode == OUTPUT__MODE_SENSORS_CALIB)
+  {
+    // Apply sensor calibration
+    compensate_sensor_errors();
+    
+    if (output_format == OUTPUT__FORMAT_BINARY)
+      output_sensors_binary();
+    else if (output_format == OUTPUT__FORMAT_TEXT)
+      output_sensors_text('C');
+  }
+  else if (output_mode == OUTPUT__MODE_SENSORS_BOTH)
+  {
+    if (output_format == OUTPUT__FORMAT_BINARY)
+    {
+      output_sensors_binary();
+      compensate_sensor_errors();
+      output_sensors_binary();
+    }
+    else if (output_format == OUTPUT__FORMAT_TEXT)
+    {
+      output_sensors_text('R');
+      compensate_sensor_errors();
+      output_sensors_text('C');
+    }
+  }
+}
+
---
+++ Razor_AHRS_v1-5/precalibrations.h
@@ -0,0 +1,176 @@
+#ifndef PRECAL
+#define PRECAL
+
+////////////////////////////////////////////////////////////////////
+// CALIBRATION DATA FOR DIFFERENT IMU BOARDS AVAILABLE AT QU-LABS //
+////////////////////////////////////////////////////////////////////
+
+/////////////
+// default //
+/////////////
+
+  
+  #define ACCEL_X_MIN ((float) -250)
+  #define ACCEL_X_MAX ((float) 250)
+  #define ACCEL_Y_MIN ((float) -250)
+  #define ACCEL_Y_MAX ((float) 250)
+  #define ACCEL_Z_MIN ((float) -250)
+  #define ACCEL_Z_MAX ((float) 250)
+
+  // "magn x,y,z (min/max) = -511.00/581.00  -516.00/568.00  -489.00/486.00"
+  #define MAGN_X_MIN ((float) -500)
+  #define MAGN_X_MAX ((float) 500)
+  #define MAGN_Y_MIN ((float) -500)
+  #define MAGN_Y_MAX ((float) 500)
+  #define MAGN_Z_MIN ((float) -500)
+  #define MAGN_Z_MAX ((float) 500)
+  
+  #define CALIBRATION__MAGN_USE_EXTENDED true
+  const float magn_ellipsoid_center[3] = {0.0, 0.0, 0.0};
+  const float magn_ellipsoid_transform[3][3] = {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}};
+  
+  #define GYRO_AVERAGE_OFFSET_X ((float) 0.0)
+  #define GYRO_AVERAGE_OFFSET_Y ((float) 0.0)
+  #define GYRO_AVERAGE_OFFSET_Z ((float) 0.0)
+
+  float yaw_up = 0.0; // noted yawn value
+  float pitch_up = 0.0; // noted pitch value
+  float roll_up = 0.0; // noted roll value
+
+
+//////////
+// 0520 //
+//////////
+
+  /*
+  #define ACCEL_X_MIN ((float) -275)
+  #define ACCEL_X_MAX ((float) 269)
+  #define ACCEL_Y_MIN ((float) -283)
+  #define ACCEL_Y_MAX ((float) 287)
+  #define ACCEL_Z_MIN ((float) -286)
+  #define ACCEL_Z_MAX ((float) 244)
+
+  // "magn x,y,z (min/max) = -511.00/581.00  -516.00/568.00  -489.00/486.00"
+  #define MAGN_X_MIN ((float) -255)
+  #define MAGN_X_MAX ((float) 566)
+  #define MAGN_Y_MIN ((float) -439)
+  #define MAGN_Y_MAX ((float) 503)
+  #define MAGN_Z_MIN ((float) -465)
+  #define MAGN_Z_MAX ((float) 288)
+  
+  #define CALIBRATION__MAGN_USE_EXTENDED true
+  const float magn_ellipsoid_center[3] = {157.716, -47.7701, -88.7542};
+  const float magn_ellipsoid_transform[3][3] = {{0.910957, 0.0126340, 0.0110979}, {0.0126340, 0.905756, 0.0220118}, {0.0110979, 0.0220118, 0.992599}};
+  
+  #define GYRO_AVERAGE_OFFSET_X ((float) -47.5)
+  #define GYRO_AVERAGE_OFFSET_Y ((float) 52.6)
+  #define GYRO_AVERAGE_OFFSET_Z ((float) 2.95)
+
+  float yaw_up = 0.0; // noted yawn value
+  float pitch_up = 0.0; // noted pitch value
+  float roll_up = 0.0; // noted roll value
+  */
+
+
+//////////
+// 0499 //
+//////////
+
+  /*
+  #define ACCEL_X_MIN ((float) -288)
+  #define ACCEL_X_MAX ((float) 232)
+  #define ACCEL_Y_MIN ((float) -251)
+  #define ACCEL_Y_MAX ((float) 276)
+  #define ACCEL_Z_MIN ((float) -264)
+  #define ACCEL_Z_MAX ((float) 250)
+  
+  #define CALIBRATION__MAGN_USE_EXTENDED true
+  const float magn_ellipsoid_center[3] = {226.878, -86.5913, -79.5690};
+  const float magn_ellipsoid_transform[3][3] = {{0.884645, 0.0170150, 0.00830839}, {0.0170150, 0.924206, 0.00954216}, {0.00830839, 0.00954216, 0.997819}};
+  
+  #define GYRO_AVERAGE_OFFSET_X ((float) -60.9)
+  #define GYRO_AVERAGE_OFFSET_Y ((float) 55.37)
+  #define GYRO_AVERAGE_OFFSET_Z ((float) 14.7)
+
+  float yaw_up = -80; // noted yawn value
+  float pitch_up = 22; // noted pitch value
+  float roll_up = 154; // noted roll value
+  */
+
+
+   
+///////////
+// IMU 2 //
+///////////
+
+  /*
+  #define ACCEL_X_MIN ((float) -280)
+  #define ACCEL_X_MAX ((float) 274)
+  #define ACCEL_Y_MIN ((float) -300)
+  #define ACCEL_Y_MAX ((float) 298)
+  #define ACCEL_Z_MIN ((float) -271)
+  #define ACCEL_Z_MAX ((float) 255)
+
+  #define CALIBRATION__MAGN_USE_EXTENDED true // IMU 2
+  const float magn_ellipsoid_center[3] = {173.289, -41.4755, -84.9911};
+  const float magn_ellipsoid_transform[3][3] = {{0.886106, 0.00978716, 0.0179369}, {0.00978716, 0.893935, 0.0192148}, {0.0179369, 0.0192148, 0.993081}};
+
+  #define GYRO_AVERAGE_OFFSET_X ((float) -51.67)
+  #define GYRO_AVERAGE_OFFSET_Y ((float) -11.02)
+  #define GYRO_AVERAGE_OFFSET_Z ((float) -2.08)
+  */
+
+
+
+//////////
+// 0485 //
+//////////
+
+  // calibration for Sennheiser HD485 (Inv.Nr. 0001)
+  /*
+  #define ACCEL_X_MIN ((float) -273)
+  #define ACCEL_X_MAX ((float) 263)
+  #define ACCEL_Y_MIN ((float) -269)
+  #define ACCEL_Y_MAX ((float) 269)
+  #define ACCEL_Z_MIN ((float) -267)
+  #define ACCEL_Z_MAX ((float) 252)
+
+  #define CALIBRATION__MAGN_USE_EXTENDED true
+  const float magn_ellipsoid_center[3] = {56.5795, -2.71042, 2.62838};
+  const float magn_ellipsoid_transform[3][3] = {{0.908602, 0.00707055, -0.00369838}, {0.00707055, 0.923283, 0.00415908}, {-0.00369838, 0.00415908, 0.999653}};
+
+  #define GYRO_AVERAGE_OFFSET_X ((float) -6.99)
+  #define GYRO_AVERAGE_OFFSET_Y ((float) -13.42)
+  #define GYRO_AVERAGE_OFFSET_Z ((float) -15.99)
+
+  float yaw_up = 7; // noted yawn value
+  float pitch_up = 5; // noted pitch value
+  float roll_up = 5; // noted roll value
+  */
+
+//////////
+// 0484 // NOCHT NICHT FERTIG!!!
+//////////
+
+  // calibration for Sennheiser HD485 (Inv.Nr. 0001)
+  /*
+  #define ACCEL_X_MIN ((float) -273)
+  #define ACCEL_X_MAX ((float) 263)
+  #define ACCEL_Y_MIN ((float) -269)
+  #define ACCEL_Y_MAX ((float) 269)
+  #define ACCEL_Z_MIN ((float) -267)
+  #define ACCEL_Z_MAX ((float) 252)
+
+  #define CALIBRATION__MAGN_USE_EXTENDED true
+  const float magn_ellipsoid_center[3] = {56.5795, -2.71042, 2.62838};
+  const float magn_ellipsoid_transform[3][3] = {{0.908602, 0.00707055, -0.00369838}, {0.00707055, 0.923283, 0.00415908}, {-0.00369838, 0.00415908, 0.999653}};
+
+  #define GYRO_AVERAGE_OFFSET_X ((float) -6.99)
+  #define GYRO_AVERAGE_OFFSET_Y ((float) -13.42)
+  #define GYRO_AVERAGE_OFFSET_Z ((float) -15.99)
+
+  float yaw_up = 5; // noted yawn value
+  float pitch_up = 0; // noted pitch value
+  float roll_up = -1; // noted roll value
+  */
+# endif  
